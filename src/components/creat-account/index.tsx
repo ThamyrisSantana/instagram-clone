4c2b0231ed9b989bfd13e16781edf06229f85ae3
Thamyris <thamyrissatana3@gmail.com>
2021-06-30 17:51:09 -0300

    css responsive added

--- src/components/creat-account/index.tsx
+++ src/components/creat-account/index.tsx
@@ -2,10 +2,12 @@ import "./styles.css";
 
 export default function CreactAccount() {
   return (
-    <div className="account-container">
-      <p>
-        <a href="a">Trocar de conta</a> ou <a href="a">Cadastre-se</a>
-      </p>
+    <div className="account-wrap">
+      <div className="account-container">
+        <p>
+          <a href="a">Trocar de conta</a> ou <a href="a">Cadastre-se</a>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/login-box/index.tsx
+++ src/components/login-box/index.tsx
@@ -7,13 +7,15 @@ import { Button } from "../button";
 export default function Login() {
   return (
     <div className="login-wrap">
-      <img className="logo" src={logo} alt="Instagram" />
-      <img className="userImg" src={userImg} alt="Profile" />
-      <Button name="thamy.me"></Button>
-      <div>
-        <span>
-          Não é thamy.me? <a href="e">Trocar de conta</a>
-        </span>
+      <div className="login">
+        <img className="logo" src={logo} alt="Instagram" />
+        <img className="userImg" src={userImg} alt="Profile" />
+        <Button name="thamy.me"></Button>
+        <div>
+          <span>
+            Não é thamy.me? <a href="e">Trocar de conta</a>
+          </span>
+        </div>
       </div>
     </div>
   );
